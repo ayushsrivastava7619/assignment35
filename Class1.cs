824f097bc257701e97c6102febf855de1f0f0458
ayushsrivastava7619 <shivamsrivastava23112001@gmail.com>
2023-09-05 20:47:49 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment35
+{
+    internal class internalprotected
+    {
+        internal protected int ab;
+
+        internal void method()
+        {
+            ab++;
+            ab++;
+            Console.WriteLine("internal class accessed");
+            Console.WriteLine(ab);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            internalprotected obj = new internalprotected();
+            string x = Console.ReadLine();
+            obj.ab = int.Parse(x);
+            obj.method();
+            Console.Read();
+        }
+    }
+}
